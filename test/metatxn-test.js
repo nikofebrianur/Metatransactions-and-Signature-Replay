6f1380a6961d4b9329d775d19d2384d6307c6f85
Niko Febrianur <nikofebrianur@gmail.com>
2023-01-31 09:18:07 +0800

    add test for prevent signature replay

--- test/metatxn-test.js
+++ test/metatxn-test.js
@@ -61,7 +61,7 @@ describe("MetaTokenTransfer", function () {
 
 		expect(userBalance.eq(parseEther("9990"))).to.be.true;
 		expect(recipientBalance.eq(parseEther("10"))).to.be.true;
-		
+
 		nonce++;
 
 		const messageHash2 = await tokenSenderContract.getHash(
@@ -89,5 +89,69 @@ describe("MetaTokenTransfer", function () {
 		expect(recipientBalance.eq(parseEther("20"))).to.be.true;
 	});
 
-	
+	it("Should not let signature replay happen", async function () {
+		const RandomTokenFactory = await ethers.getContractFactory("RandomToken");
+		const randomTokenContract = await RandomTokenFactory.deploy();
+		await randomTokenContract.deployed();
+
+		const MetaTokenSenderFactory = await ethers.getContractFactory(
+			"TokenSender"
+		);
+		const tokenSenderContract = await MetaTokenSenderFactory.deploy();
+		await tokenSenderContract.deployed();
+
+		const [_, userAddress, relayerAddress, recipientAddress] =
+			await ethers.getSigners();
+
+		const tenThousandTokensWithDecimals = parseEther("10000");
+		const userTokenContractInstance = randomTokenContract.connect(userAddress);
+		const mintTxn = await userTokenContractInstance.freeMint(
+			tenThousandTokensWithDecimals
+		);
+		await mintTxn.wait();
+
+		const approveTxn = await userTokenContractInstance.approve(
+			tokenSenderContract.address,
+			BigNumber.from(
+				// This is uint256's max value (2^256 - 1) in hex
+				"0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+			)
+		);
+		await approveTxn.wait();
+
+		let nonce = 1;
+
+		const transferAmountOfTokens = parseEther("10");
+		const messageHash = await tokenSenderContract.getHash(
+			userAddress.address,
+			transferAmountOfTokens,
+			recipientAddress.address,
+			randomTokenContract.address,
+			nonce
+		);
+		const signature = await userAddress.signMessage(arrayify(messageHash));
+
+		const relayerSenderContractInstance =
+			tokenSenderContract.connect(relayerAddress);
+		const metaTxn = await relayerSenderContractInstance.transfer(
+			userAddress.address,
+			transferAmountOfTokens,
+			recipientAddress.address,
+			randomTokenContract.address,
+			nonce,
+			signature
+		);
+		await metaTxn.wait();
+
+		expect(
+			relayerSenderContractInstance.transfer(
+				userAddress.address,
+				transferAmountOfTokens,
+				recipientAddress.address,
+				randomTokenContract.address,
+				nonce,
+				signature
+			)
+		).to.be.revertedWith("Already executed!");
+	});
 });
